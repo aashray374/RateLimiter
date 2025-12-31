bdb6e63fd3dc5093637a3148e928dc2c8039f16b
aashray374 <aashraymahajan374@gmail.com>
2025-12-31 11:35:09 +0530

    Initial commit

---
+++ src/test/java/com/example/ratelimiter/TestRateLimiterApplication.java
@@ -0,0 +1,11 @@
+package com.example.ratelimiter;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestRateLimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(RateLimiterApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
